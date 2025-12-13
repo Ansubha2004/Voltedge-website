05348252822b0e96453e8a1d4a04985312602a79
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-12-13 21:13:12 +0530

    home page done

--- client/src/components/Timer.jsx
+++ client/src/components/Timer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-function Timer(targetdate) {
+function Timer({targetdate}) {
 
     const target = new Date(targetdate).getTime();
 
@@ -20,32 +20,35 @@ function Timer(targetdate) {
     const [timeleft, settimeleft] = useState(calculatetimeleft());
     useEffect(() => {
         const interval = setInterval(() => {
-            settimeleft(calculatetimeleft)
+            settimeleft(calculatetimeleft())
         }, 1000)
         return () => clearInterval(interval);
     }, [])
-    if (!timeLeft) {
+    if (!timeleft) {
         return (
-            <div className="text-3xl font-bold text-green-500">
+            <div className="translate-y-5 text-3xl font-bold text-green-500">
                 🎉 Countdown Finished!
             </div>
         );
     }
 
     return (
-        <div className="h-auto w-auto flex gap-4">
+        <div className="translate-y-5 h-auto w-auto flex gap-5 my-3 items-center">
             <Timebox label="DAYS" value={timeleft.days} />
-            <Timebox label="HOURS" value={timeleftleft.hours}/>\
+            <Timebox label="HOURS" value={timeleft.hours}/>
             <Timebox label="MINUTES" value={timeleft.minutes}/>
             <Timebox label="SECONDS" value={timeleft.seconds}/>
         </div>
     )
 }
 
-function Timebox(label, value) {
+function Timebox({label, value}) {
     return (
-        <div>
-            
+        <div className="w-20 h-20 border-box p-0.5 bg-gradient-to-br from-teal-500 rounded-[10px] via-purple-500 shadow-[0px_0px_12px_rgba(168,85,247,0.6)] to-red-500 ">
+            <div className="w-full h-full bg-black/70 backdrop-blur-lg rounded-[7px] text-white flex flex-col items-center justify-center">
+                <p className=" text-[2rem] leading-none space-mono-bold text-blue-400">{String(value).padStart(2,"0")}</p>
+                <p className="text-[0.7rem] text-purple-300">{label}</p>
+            </div>
         </div>
     )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,40 @@
 import React from 'react'
 import background1 from "../assets/backgroundimages/background1.png"
 import Timer from "../components/Timer.jsx"
+import { CiCalendarDate } from "react-icons/ci";
+import { FaArrowRightLong } from "react-icons/fa6";
+import Button from "../components/Button.jsx"
+import {useNavigate} from "react-router-dom"
+
+
 
 function Home() {
+
+
+  const navigate=useNavigate();
+
   return (
     <div className="w-screen h-screen relative bg-repeat-no bg-cover bg-center  " style={{ backgroundImage: `url(${background1})` }}>
-      <div className="w-screen h-screen absolute bg-black/60 flex flex-col justify-center items-center">
-        <p className="float subheading text-white exo-2-font drop-shadow-[0_0_7px_rgba(255,255,255,0.8)] ">27TH FEBRUARY, 2026 - 1ST MARCH, 2026</p>
-        <div className="float sekuya-regular sm:text-[500%] text-[300%] md:text-[600%] leading-none font-extrabold
-           drop-shadow-[0_0_7px_rgba(255,152,0)] bg-clip-text text-transparent bg-gradient-to-t from-orange-500 via-yellow-500 to-orange-500 text-center ">
-            <p>VOLT-EDGE</p>
-            <p className="">2026</p>
+      <div className="w-screen h-screen absolute bg-black/60 flex flex-col  justify-center items-center">
+        <p className="translate-y-1 subheading text-white exo-2-font drop-shadow-[0_0_7px_rgba(255,255,255,0.8)] ">EE and EEE Department Presents</p>
+        <p className="translate-y-5 float sekuya-regular sm:text-[400%] text-[300%] md:text-[500%] leading-none mb-5 font-extrabold
+           drop-shadow-[0_0_7px_rgba(255,152,0)] bg-clip-text text-transparent bg-gradient-to-t tracking-widest from-orange-500 via-yellow-500 to-orange-500 text-center ">
+            VOLT-EDGE
+        </p>
+        <Timer targetdate="2026-02-27T00:00:00"/>
+        <div className="flex translate-y-5 justify-center items-center text-[1rem] gap-2 oxanium pop p-2 bg-black/40  border border-white/40 my-2 backdrop-blur-sm">
+          <CiCalendarDate className="text-2xl -translate-y-0.5 text-orange-500 "/>
+          <p className="text-green-400  [text-shadow:0_0px_3px_rgba(0,255,0,0.8)] ">27TH FEBRUARY, 2026</p>
+          <FaArrowRightLong className="text-white "/>
+          <p className="text-green-400 [text-shadow:0_0px_3px_rgba(0,255,0,0.8)]">1ST MARCH, 2026</p>
         </div>
+        <div className="translate-y-5 w-auto h-auto flex justify-center items-center space-x-[10%] mt-4">
+          <Button click={()=>navigate("")} Content="REGISTER NOW" themecss="btn1 "/>
+          <Button click={()=>navigate("")} Content="VIEW SCHEDULE" themecss="btn2 "/>
+        </div>
+        
       </div>
-      <p></p>
+      
     </div>
   )
 }
