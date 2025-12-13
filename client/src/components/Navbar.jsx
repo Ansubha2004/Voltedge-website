fbe5d656f80033e3c25f764ae2152feda12d4edc
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-12-13 14:52:34 +0530

    home page day1

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import voltedgelogo from "../assets/logos/voltedgelogo.png"
+import darkiemlogo from "../assets/logos/darkiemlogo.png"
 import {Link} from "react-router-dom"
 function Navbar() {
   return (
     <div className="Navbar text-white">
-      <img src={voltedgelogo} className="h-25" alt="" />
-      <div className="h-20 w-full border-box px-10  flex justify-evenly items-center border-[2px] border-white rounded-md">
-        <Link to="/" >HOME</Link>
-        <Link to="/about" >ABOUT</Link>
-        <Link to="/events" >EVENTS</Link>
-        <Link to="/gaming" >GAMING</Link>
-        <Link to="/hackathon" >HACKATHON</Link>
-        <Link to="/schedule" >SCHEDULE</Link>
-        <Link to="/contact" >CONTACT</Link>
+      <img src={voltedgelogo} className="h-20" alt="" />
+      <div className="paratext h-15 w-full border-box px-10  flex justify-evenly items-center border-[2px] border-white/50 rounded-l-full rounded-r-full transition-all duration-300 hover:shadow-[0px_0px_15px_rgba(255,255,255,0.50)] hover:backdrop-blur-md hover:bg-black/20 ">
+        <Link to="/" className="navbaranimation">HOME</Link>
+        <Link to="/about" className="navbaranimation">ABOUT</Link>
+        <Link to="/events" className="navbaranimation">EVENTS</Link>
+        <Link to="/gaming" className="navbaranimation">GAMING</Link>
+        <Link to="/hackathon" className="navbaranimation">HACKATHON</Link>
+        <Link to="/schedule" className="navbaranimation">SCHEDULE</Link>
+        <Link to="/contact" className="navbaranimation">CONTACT</Link>
       </div>
-      <img src={voltedgelogo} className="h-25 " alt="" />
+      <img src={darkiemlogo} className="h-17 " alt="" />
       
     </div>
   )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,8 +3,16 @@ import background1 from "../assets/backgroundimages/background1.png"
 
 function Home() {
   return (
-    <div className="w-screen h-screen relative bg-repeat-no bg-cover bg-center " style={{backgroundImage:`url(${background1})`}}>
-
+    <div className="w-screen h-screen relative bg-repeat-no bg-cover bg-center  " style={{ backgroundImage: `url(${background1})` }}>
+      <div className="w-screen h-screen absolute bg-black/60 flex flex-col justify-center items-center">
+        <p className="float subheading text-white exo-2-font drop-shadow-[0_0_7px_rgba(255,255,255,0.8)] ">Department of EE and EEE, IEM Kolkata presents</p>
+        <div className="float sekuya-regular sm:text-[500%] text-[300%] md:text-[700%] text-5xl font-extrabold
+           drop-shadow-[0_0_7px_rgba(255,152,0)] bg-clip-text text-transparent bg-gradient-to-t from-orange-500 via-yellow-500 to-orange-500 text-center ">
+            <p>VOLT-EDGE</p>
+            <p>2026</p>
+        </div>
+      </div>
+      <p></p>
     </div>
   )
 }
